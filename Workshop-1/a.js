ec500c84fada23db232f43234e1393d18a6a23fc
yeyo-san <155599984+yeyo-san@users.noreply.github.com>
2024-04-16 18:32:55 -0500

    Add files via upload

---
+++ Workshop-1/a.js
@@ -0,0 +1,51 @@
+// alert("Hola Mundo!!")
+
+// //Ejercicio 5
+// var miNumero = 10;
+// console.log(miNumero);
+
+
+// //Ejercicio 6
+// var miBooleano = true;
+// console.log(typeof(miBooleano));
+
+// //Ejercicio 7
+// var miCadena = "Mi papa se llama edgar";
+// console.log(miCadena.length);
+
+// //Ejercicio 8
+// var miArray = ["hola", 23, [23, 22]];
+// console.log(miArray);
+
+// //Ejercicio 9
+// var miObjeto = {
+//     usuario: "Yeyo",
+//     nacionalidad: "Bellaco"
+// }; 
+// console.log(miObjeto;
+
+//Ejercicio 10
+// var nombreU = prompt("Cual es tu nombre?");
+// alert("hola " + nombreU);
+
+//Ejercicio 11
+// var opcion = confirm("Te gusta JavaScript?");
+
+// if (opcion == true) {
+//     alert("Eso es todo")
+// }
+
+// else{
+//     alert("Mero bobito")
+// }
+
+//Ejercicio 12
+// console.warn("Esto es solo una practica");
+
+//Ejercicio 13
+// console.error("De concatenacion")
+// var array2 = [1,2,3,4,5];
+// console.table(array2)
+
+
+
